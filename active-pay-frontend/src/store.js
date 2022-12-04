9bd08ba538227af93465eb1137c614cae3bb25c0
Jahnavi Gangarapu <gangarapu.j@northeastern.edu>
2022-12-04 13:54:10 -0500

    redux

---
+++ active-pay-frontend/src/store.js
@@ -0,0 +1,42 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+import {
+  userLoginReducer,
+  userRegisterReducer,
+  userDetailsReducer,
+  userUpdateProfileReducer,
+} from './reducers/userReducers';
+
+import {
+  rewardPointsReducer,
+  allCouponsReducer,
+} from './reducers/rewardReducer';
+
+const reducer = combineReducers({
+  userLogin: userLoginReducer,
+  userRegister: userRegisterReducer,
+  userDetails: userDetailsReducer,
+  userUpdateProfile: userUpdateProfileReducer,
+  rewardPoints: rewardPointsReducer,
+  allCoupons: allCouponsReducer,
+});
+
+const userInfoFromLocalStorage = localStorage.getItem('userInfo')
+  ? JSON.parse(localStorage.getItem('userInfo'))
+  : null;
+
+const initialState = {
+  userLogin: { userInfo: userInfoFromLocalStorage },
+};
+
+const middleware = [thunk];
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
