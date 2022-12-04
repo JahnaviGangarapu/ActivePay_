d7ca2e0258320cb73cb43e586c86a590fa243413
Madhura Kurhadkar <kurhadkar.m@northeastern.edu>
2022-12-04 14:33:20 -0500

    Added the cards component

---
+++ active-pay-frontend/src/actions/CardType.js
@@ -0,0 +1,6 @@
+//author: Madhura Kurhadkar
+
+export const AddNewCard = 'AddNewCard';
+export const GetAllCards = 'GetAllCards';
+export const GetCardById = 'GetCardById';
+export const DeleteCard = 'DeleteCard';
