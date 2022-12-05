60c8a5b64d203918a7e6d5ceb599b5ab89791fee
Jahnavi Gangarapu <gangarapu.j@northeastern.edu>
2022-12-05 01:00:20 -0500

    mainscreen

---
+++ active-pay-frontend/src/screens/HomeScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Branding from "../components/Branding";
+//import BrandCarousel from "../components/BrandCarousel";
+
+const HomeScreen = () => {
+  return (
+    <>
+      <Branding />
+      {/* // eslint-disable-next-line */}
+      {/* <BrandCarousel /> */}
+    </>
+  );
+};
+
+export default HomeScreen;
