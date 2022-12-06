292711278067f9354d39e78197be628023b5d79b
Jahnavi Gangarapu <gangarapu.j@northeastern.edu>
2022-12-06 17:33:26 -0500

    constant bugs

--- active-pay-frontend/src/reducers/rewardReducer.js
+++ active-pay-frontend/src/reducers/rewardReducer.js
@@ -1,5 +1,4 @@
 import {
-<<<<<<< HEAD
   ALL_COUPONS_FAIL,
   ALL_COUPONS_REQUEST,
   ALL_COUPONS_RESET,
@@ -36,28 +35,3 @@ export const allCouponsReducer = (state = { coupons: [] }, action) => {
       return state;
   }
 };
-=======
-    ALL_COUPONS_FAIL,
-    ALL_COUPONS_REQUEST,
-    ALL_COUPONS_RESET,
-    ALL_COUPONS_SUCCESS,
-    REWARD_POINTS_FAIL,
-    REWARD_POINTS_REQUEST,
-    REWARD_POINTS_SUCCESS,
-  } from '../constants/rewardConstant.js';
-
-  //here is the reducer for rewards points of that particular user.
-
-export const rewardPointsReducer = (state = {}, action) => {
-    switch (action.type) {
-      case REWARD_POINTS_REQUEST:
-        return { loading: true };
-      case REWARD_POINTS_SUCCESS:
-        return { loading: false, coins: action.payload };
-      case REWARD_POINTS_FAIL:
-        return { loading: false, error: action.payload };
-      default:
-        return state;
-    }
-  };
->>>>>>> 038e89ce69a855550d9d0751b071e989070ccb22
