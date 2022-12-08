a0d2523c76104c5ebd223778af68f2ffd2479d46
Jahnavi Gangarapu <gangarapu.j@northeastern.edu>
2022-12-08 11:27:56 -0500

    Documentation1

--- active-pay-frontend/src/actions/paymnetActions.js
+++ active-pay-frontend/src/actions/paymnetActions.js
@@ -1,12 +1,16 @@
-import axios from '../axios';
+import axios from '../axios';  // importing axios
 
 import {
   PAYMENT_FAIL,
+
+
   PAYMENT_REQUEST,
   PAYMENT_SUCCESS,
-} from '../constants/paymentConstants';
+} from '../constants/paymentConstants'; // fetchning data from paymnent constants
 
-export const payAmount = (cardNo, amount) => async (dispatch, getState) => {
+export const payAmount = (cardNo, amount) => async (dispatch, getState) => {   // payment code
+
+
   try {
     dispatch({ type: PAYMENT_REQUEST });
     const {
@@ -14,14 +18,18 @@ export const payAmount = (cardNo, amount) => async (dispatch, getState) => {
     } = getState();
 
     const config = {
+
+
       headers: {
         'Content-Type': 'application/json',
-        Authorization: `Bearer ${userInfo.token}`,
+        Authorization: `Bearer ${userInfo.token}`,  // Bearer token is used in the postman
       },
     };
 
+
     const { data } = await axios.post(
-      `/api/cards/${cardNo}/pay`,
+
+      `/api/cards/${cardNo}/pay`,  // api for card payments
       { amount },
       config
     );
@@ -29,11 +37,14 @@ export const payAmount = (cardNo, amount) => async (dispatch, getState) => {
     dispatch({ type: PAYMENT_SUCCESS, payload: data });
   } catch (err) {
     dispatch({
+
       type: PAYMENT_FAIL,
       payload:
         err.response && err.response.data.message
+
           ? err.response.data.message
           : err.message,
+          
     });
   }
 };
